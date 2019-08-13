c71d8cfcc8f49f3a81815f336d8b0e141710a50f
lixinxin <895330766@qq.com>
2019-08-13 16:46:33 +0800

    Initial commit

---
+++ app/src/main/java/com/lixinxinlove/jetpack/fragment/Fragment1.kt
@@ -0,0 +1,45 @@
+package com.lixinxinlove.jetpack.fragment
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.navOptions
+import com.lixinxinlove.jetpack.R
+
+
+class Fragment1 : Fragment() {
+
+
+    lateinit var btn: Button
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        val view = inflater.inflate(R.layout.fragment1, container, false)
+
+        btn = view.findViewById(R.id.btn_1)
+
+        btn.setOnClickListener {
+
+            val navOption = navOptions {
+                anim {
+                    enter = R.anim.common_slide_in_right
+                    exit = R.anim.common_slide_out_left
+                    popEnter = R.anim.common_slide_in_left
+                    popExit = R.anim.common_slide_out_right
+                }
+            }
+            // 参数设置
+            val bundle = Bundle()
+            bundle.putString("name","TeaOf")
+            findNavController().navigate(R.id.f2, bundle,navOption)
+        }
+
+        return view
+    }
+
+}
---
+++ app/src/main/java/com/lixinxinlove/jetpack/fragment/Fragment3.kt
@@ -0,0 +1,18 @@
+package com.lixinxinlove.jetpack.fragment
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.lixinxinlove.jetpack.R
+
+
+
+class Fragment3 : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment3, container, false)
+    }
+}
