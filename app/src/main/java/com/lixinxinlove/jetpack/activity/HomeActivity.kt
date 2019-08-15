4e077a44aa77ae104971de03d23ebaa42fb26c6f
lixinxin <895330766@qq.com>
2019-08-15 14:37:23 +0800

    跳转activity

---
+++ app/src/main/java/com/lixinxinlove/jetpack/activity/HomeActivity.kt
@@ -0,0 +1,13 @@
+package com.lixinxinlove.jetpack.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.lixinxinlove.jetpack.R
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
--- app/src/main/java/com/lixinxinlove/jetpack/fragment/Fragment1.kt
+++ app/src/main/java/com/lixinxinlove/jetpack/fragment/Fragment1.kt
@@ -16,12 +16,14 @@ class Fragment1 : Fragment() {
 
 
     lateinit var btn: Button
+    lateinit var btnOther: Button
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         val view = inflater.inflate(R.layout.fragment1, container, false)
 
         btn = view.findViewById(R.id.btn_1)
+        btnOther = view.findViewById(R.id.btn_other)
 
         btn.setOnClickListener {
 
@@ -37,6 +39,22 @@ class Fragment1 : Fragment() {
             val bundle = Bundle()
             bundle.putString("name","TeaOf")
             findNavController().navigate(R.id.f2, bundle,navOption)
+        }
+
+        btnOther.setOnClickListener {
+
+            val navOption = navOptions {
+                anim {
+                    enter = R.anim.common_slide_in_right
+                    exit = R.anim.common_slide_out_left
+                    popEnter = R.anim.common_slide_in_left
+                    popExit = R.anim.common_slide_out_right
+                }
+            }
+            // 参数设置
+            val bundle = Bundle()
+            bundle.putString("name","TeaOf")
+            findNavController().navigate(R.id.home_activity, bundle,navOption)
         }
 
         return view
